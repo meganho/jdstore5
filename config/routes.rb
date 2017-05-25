2556bf01addcac408716a693e5f4105358eb63fc
meganho <bbeeaanns@gmail.com>
2017-05-25 16:22:16 +0200

    copy from jdstore2 & remove application.yml file

---
+++ config/routes.rb
@@ -0,0 +1,46 @@
+Rails.application.routes.draw do
+  root 'products#index'
+  devise_for :users
+    namespace :admin do
+           resources :products do
+        member do
+          patch :move_up
+          patch :move_down
+        end
+      end
+          resources :orders do
+      member do
+        post :cancel
+        post :ship
+        post :shipped
+        post :return
+      end
+    end
+    end
+
+         resources :products do
+     member do
+       post :add_to_cart
+     end
+   end
+     resources :carts do
+     collection do
+       delete :clean
+       post :checkout
+     end
+   end
+
+   resources :cart_items
+
+   namespace :account do
+  resources :orders
+end
+
+        resources :orders do
+       member do
+         post :pay_with_alipay
+         post :pay_with_wechat
+         post :apply_to_cancel
+       end
+     end
+  end
